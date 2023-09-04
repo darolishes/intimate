a77ca956a3562fd85d6793faee2b47a410b6a2b1
Dawid Rogaczewski <dawid@daro.de>
2023-09-04 08:48:14 +0200

    up

---
+++ IntimateTales_User/roleplay_preferences.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Roleplay Preferences Module for IntimateTales User Plugin
+ * 
+ * This module handles the roleplay preferences of the user.
+ * 
+ * @package IntimateTales_User_Plugin
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Fetch roleplay preferences from ACF Integration Plugin
+ * 
+ * @return array
+ */
+function fetch_roleplay_preferences() {
+    // Fetch roleplay preferences from ACF Integration Plugin
+    $roleplay_preferences = get_field('roleplay_preferences', 'user_' . get_current_user_id());
+
+    return $roleplay_preferences;
+}
+
+/**
+ * Update roleplay preferences
+ * 
+ * @param array $new_preferences
+ * @return void
+ */
+function update_roleplay_preferences($new_preferences) {
+    // Update roleplay preferences in user meta
+    update_user_meta(get_current_user_id(), 'roleplay_preferences', $new_preferences);
+
+    // Send success message
+    echo json_encode(array('message' => 'updateSuccess'));
+}
+
+// Fetch roleplay preferences on page load
+add_action('wp_loaded', 'fetch_roleplay_preferences');
+
+// Update roleplay preferences on form submission
+add_action('wp_ajax_update_roleplay_preferences', 'update_roleplay_preferences');
